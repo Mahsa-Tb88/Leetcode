b61f0d6dca11b29d75d64a1652f59a4275effaf8
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-07-16 18:32:52 -0700

    I did it!

--- findFirstIndex.js
+++ findFirstIndex.js
@@ -2,3 +2,31 @@
 // Given two strings needle and haystack,
 //  return the index of the first occurrence of needle in haystack,
 //  or -1 if needle is not part of haystack
+
+var strStr = function (haystack, needle) {
+  const isIncluded = haystack.includes(needle);
+
+  if (isIncluded) {
+    let w = "";
+    for (let i = 0; i < haystack.length; i++) {
+      if (haystack[i] == needle[0]) {
+        for (let j = 0; j < needle.length; j++) {
+          if (needle[j] !== haystack[i + j]) {
+            break;
+          }
+          w = w + needle[j];
+        }
+      }
+      if (w == needle) {
+        return i;
+      } else {
+        w = "";
+      }
+    }
+  } else {
+    return -1;
+  }
+};
+haystack = "mississippi";
+needle = "issip";
+console.log(strStr(haystack, needle));
