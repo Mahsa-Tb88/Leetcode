0c8e538f8a9722c5982dd7ff2cdcfa11bf8d54d4
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-07-16 17:43:36 -0700

    I did it!

--- RemoveDupEl.js
+++ RemoveDupEl.js
@@ -1,17 +1,17 @@
-// const a = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
-const a = [1, 1, 2];
+const a = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+// const a = [1, 1, 2];
 
 var removeDuplicates = function (nums) {
   let k = 1;
   for (let i = 1; i < nums; i++) {
     if (nums[i] !== nums[i - 1]) {
       nums[k] = nums[i];
+
       k++;
     }
-    
   }
 
-  return k;
+  return { k, nums };
 };
 
 console.log(removeDuplicates(a));
--- removeElement.js
+++ removeElement.js
@@ -1,4 +1,4 @@
-// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
 // The order of the elements may be changed.
 //  Then return the number of elements in nums which are not equal to val.
 
@@ -7,3 +7,22 @@
 // Change the array nums such that the first k elements of nums contain the elements which are not equal to val.
 //  The remaining elements of nums are not important as well as the size of nums.
 // Return k.
+
+var removeElement = function (nums, val) {
+  let k = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i];
+      k++;
+    }
+    if (i == nums.length - 1 && nums[i] == val) {
+      nums[i] = nums[0];
+    }
+  }
+  return k;
+};
+
+const nums = [0, 1, 2, 2, 3, 0, 4, 2];
+const val = 2;
+
+console.log(removeElement(nums, val));
