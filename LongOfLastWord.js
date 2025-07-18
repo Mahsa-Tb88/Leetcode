28b49a12b6cc52062155f04398e61eebaa84c1d5
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-07-17 23:11:58 -0700

    done

--- LongOfLastWord.js
+++ LongOfLastWord.js
@@ -4,16 +4,17 @@
 
 // A word is a maximal substring consisting of non-space characters only.
 
- 
-
 // Example 1:
 
 // Input: s = "Hello World"
 // Output: 5
 // Explanation: The last word is "World" with length 5.
 
-
-
-var lengthOfLastWord = function(s) {
-    
+var lengthOfLastWord = function (s) {
+  let list = s.split(" ");
+  list = list.filter((l) => l !== "");
+  return list[list.length - 1].length;
 };
+
+s = "Today is a nice day    ";
+console.log(lengthOfLastWord(s));
