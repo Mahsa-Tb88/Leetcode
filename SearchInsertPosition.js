45c1448cd8812310a242fb5f38a02738740be236
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-07-17 22:58:37 -0700

    done

--- SearchInsertPosition.js
+++ SearchInsertPosition.js
@@ -4,8 +4,6 @@
 
 // You must write an algorithm with O(log n) runtime complexity.
 
- 
-
 // Example 1:
 
 // Input: nums = [1,3,5,6], target = 5
@@ -18,4 +16,29 @@
 
 // Input: nums = [1,3,5,6], target = 7
 // Output: 4
- 
+
+var searchInsert = function (nums, target) {
+  const findTarget = nums.find((num) => num == target);
+
+  if (findTarget) {
+    return nums.indexOf(target);
+  } else {
+    let a;
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] < target && target < nums[i + 1]) {
+        return i + 1;
+      }
+      if (target > nums[nums.length - 1]) {
+        return nums.length;
+      }
+      if (target < nums[0]) {
+        return 0;
+      }
+    }
+  }
+};
+
+const nums = [1, 3, 4, 6];
+const target = 7;
+
+console.log(searchInsert(nums, target));
